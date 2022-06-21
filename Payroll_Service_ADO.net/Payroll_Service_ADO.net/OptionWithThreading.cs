3b4507899b3cc3f7a1d3ef28d8f13941339ca518
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-21 16:27:37 +0530

    [Roshni]-Add Multiple Employee

---
+++ Payroll_Service_ADO.net/Payroll_Service_ADO.net/OptionWithThreading.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll_Service_ADO.net
+{
+    public class OptionWithThreading
+    {
+        public List<EmployeePayroll2> Employeeinfo = new List<EmployeePayroll2>();
+        public void addEmployeeToPayRoll(List<EmployeePayroll2> employeeinfo)
+        {
+            employeeinfo.ForEach(employee =>
+            {
+                Console.WriteLine("Employee being added:" + employee.FirstName);
+                this.addEmployeePayRoll(employee);
+                Console.WriteLine("Employee Added :" + employee.FirstName);
+            });
+        }
+
+
+        public void addEmployeePayRoll(EmployeePayroll2 emp)
+        {
+            Employeeinfo.Add(emp);
+        }
+    }
+}
--- Payroll_Service_ADO.net/Payroll_Service_ADO.net/Program.cs
+++ Payroll_Service_ADO.net/Payroll_Service_ADO.net/Program.cs
@@ -8,7 +8,7 @@ repo.GetAllEmployeePayroll();
 repo.AddEmployee(payroll);
 while (true)
 {
-    Console.WriteLine("Choose the option : \n1)Create and retrieve values from Database\n2)Add Employee Details\n3)Update salary\n4)Aggregate Operations\n5)Get Employee details Of Date Range");
+    Console.WriteLine("Choose the option : \n1)Create and retrieve values from Database\n2)Add Employee Details\n3)Update salary\n4)Aggregate Operations\n5)Get Employee details Of Date Range\n6)Operation With and without Threads");
     int option = Convert.ToInt16(Console.ReadLine());
     switch (option)
     {
@@ -17,7 +17,7 @@ while (true)
             break;
         case 2:
             payroll.Name = "Terissa";
-           // payroll.StartDate = "2022-03-12";
+            //payroll.StartDate = "2022-03-12";
             payroll.Gender = 'F';
             payroll.PhoneNumber = "8877665544";
             payroll.Address = "Mumbai";
@@ -31,7 +31,6 @@ while (true)
             break;
         case 3:
             Console.WriteLine("Enter Name of the Employee : ");
-            //string Name = Console.ReadLine);
             repo.UpdateBasicPay();
             double salary = repo.UpdateBasicPay();
             Console.Write("Updated Salary of Employee : ");
@@ -52,6 +51,20 @@ while (true)
             break;
         case 5:
             repo.GetEmployeesByDateRange();
+            break;
+        case 6:
+            List<EmployeePayroll2> employeeDetails2 = new List<EmployeePayroll2>();
+            employeeDetails2.Add(new EmployeePayroll2(EmployeeID: 2, FirstName: "Roshni", LastName: "Adatrao", Gender: "Female", StartDate: DateTime.Now, Company: "TCS", Departent: "Testing", Address: "Pune", BasicPay: 40000, Deductions: 1000, TaxablePay: 10000, IncomeTax: 500, NetPay: 15000));
+            employeeDetails2.Add(new EmployeePayroll2(EmployeeID: 1, FirstName: "Mahesh", LastName: "Kulkarni", Gender: "Male", StartDate: DateTime.Now, Company: "TCS", Departent: "Testing", Address: "Mumbai", BasicPay: 40000, Deductions: 1000, TaxablePay: 1000, IncomeTax: 500, NetPay: 15000));
+            employeeDetails2.Add(new EmployeePayroll2(EmployeeID: 3, FirstName: "Vivek", LastName: "Patil", Gender: "Male", StartDate: DateTime.Now, Company: "TCS", Departent: "Testing", Address: "Nashik", BasicPay: 40000, Deductions: 1000, TaxablePay: 1000, IncomeTax: 500, NetPay: 15000));
+            employeeDetails2.Add(new EmployeePayroll2(EmployeeID: 4, FirstName: "Pradnya", LastName: "Chavan", Gender: "Female", StartDate: DateTime.Now, Company: "TCS", Departent: "Testing", Address: "Mumbai", BasicPay: 40000, Deductions: 1000, TaxablePay: 1000, IncomeTax: 500, NetPay: 15000));
+            employeeDetails2.Add(new EmployeePayroll2(EmployeeID: 5, FirstName: "Lavanya", LastName: "Kapoor", Gender: "Female", StartDate: DateTime.Now, Company: "TCS", Departent: "Testing", Address: "Banglore", BasicPay: 40000, Deductions: 1000, TaxablePay: 1000, IncomeTax: 500, NetPay: 15000));
+
+            OptionWithThreading operationWIthThreads = new OptionWithThreading();
+            DateTime StartdateTime = DateTime.Now;
+            operationWIthThreads.addEmployeeToPayRoll(employeeDetails2);
+            DateTime StopDataTime = DateTime.Now;
+            Console.WriteLine("Duration without Thread: " + (StopDataTime - StartdateTime));
             break;
         default:
             Console.WriteLine("Please choose the correct option");
