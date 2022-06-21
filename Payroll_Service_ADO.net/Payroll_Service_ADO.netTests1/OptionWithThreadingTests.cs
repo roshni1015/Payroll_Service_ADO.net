9d3552da620cc6fb4f2f82dd18a1527fa44e5244
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-21 16:41:21 +0530

    [Roshni]-Using Multiple Threading

--- Payroll_Service_ADO.net/Payroll_Service_ADO.netTests1/OptionWithThreadingTests.cs
+++ Payroll_Service_ADO.net/Payroll_Service_ADO.netTests1/OptionWithThreadingTests.cs
@@ -12,7 +12,7 @@ namespace Payroll_Service_ADO.net.Tests
     public class OptionWithThreadingTests
     {
         [TestMethod()]
-        public void addEmployeeToPayRollTest()
+        public void AddingEmployeeTest()
         {
             List<EmployeePayroll2> employeeDetails2 = new List<EmployeePayroll2>();
             employeeDetails2.Add(new EmployeePayroll2(EmployeeID: 2, FirstName: "Roshni", LastName: "Adatrao", Gender: "Female", StartDate: DateTime.Now, Company: "TCS", Departent: "Testing", Address: "Pune", BasicPay: 40000, Deductions: 1000, TaxablePay: 10000, IncomeTax: 500, NetPay: 15000));
@@ -23,9 +23,33 @@ namespace Payroll_Service_ADO.net.Tests
 
             OptionWithThreading operationWIthThreads = new OptionWithThreading();
             DateTime StartdateTime = DateTime.Now;
-            operationWIthThreads.addEmployeeToPayRoll(employeeDetails2);
+            operationWIthThreads.AddingEmployee(employeeDetails2);
             DateTime StopDataTime = DateTime.Now;
             Console.WriteLine("Duration without Thread: " + (StopDataTime - StartdateTime));
+        }
+
+        [TestMethod()]
+        public void AddingWithThreadTest()
+        {
+            List<EmployeePayroll2> employeeDetails2 = new List<EmployeePayroll2>();
+            employeeDetails2.Add(new EmployeePayroll2(EmployeeID: 2, FirstName: "Roshni", LastName: "Adatrao", Gender: "Female", StartDate: DateTime.Now, Company: "TCS", Departent: "Testing", Address: "Pune", BasicPay: 40000, Deductions: 1000, TaxablePay: 10000, IncomeTax: 500, NetPay: 15000));
+            employeeDetails2.Add(new EmployeePayroll2(EmployeeID: 1, FirstName: "Mahesh", LastName: "Kulkarni", Gender: "Male", StartDate: DateTime.Now, Company: "TCS", Departent: "Testing", Address: "Mumbai", BasicPay: 40000, Deductions: 1000, TaxablePay: 1000, IncomeTax: 500, NetPay: 15000));
+            employeeDetails2.Add(new EmployeePayroll2(EmployeeID: 3, FirstName: "Vivek", LastName: "Patil", Gender: "Male", StartDate: DateTime.Now, Company: "TCS", Departent: "Testing", Address: "Nashik", BasicPay: 40000, Deductions: 1000, TaxablePay: 1000, IncomeTax: 500, NetPay: 15000));
+            employeeDetails2.Add(new EmployeePayroll2(EmployeeID: 4, FirstName: "Pradnya", LastName: "Chavan", Gender: "Female", StartDate: DateTime.Now, Company: "TCS", Departent: "Testing", Address: "Mumbai", BasicPay: 40000, Deductions: 1000, TaxablePay: 1000, IncomeTax: 500, NetPay: 15000));
+            employeeDetails2.Add(new EmployeePayroll2(EmployeeID: 5, FirstName: "Lavanya", LastName: "Kapoor", Gender: "Female", StartDate: DateTime.Now, Company: "TCS", Departent: "Testing", Address: "Banglore", BasicPay: 40000, Deductions: 1000, TaxablePay: 1000, IncomeTax: 500, NetPay: 15000));
+
+
+            OptionWithThreading operationWIthThreads = new OptionWithThreading();
+            DateTime StartdateTime = DateTime.Now;
+            operationWIthThreads.AddingEmployee(employeeDetails2);
+            DateTime StopDataTime = DateTime.Now;
+            Console.WriteLine("Duration without Thread: " + (StopDataTime - StartdateTime));
+        
+            DateTime StartdateTimeThread = DateTime.Now;
+            operationWIthThreads.AddingWithThread(employeeDetails2);
+            DateTime StopDataTimeThread = DateTime.Now;
+            Console.WriteLine("Duration with Thread: " + (StopDataTimeThread - StartdateTimeThread));
+
         }
     }
 }
